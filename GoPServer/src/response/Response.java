7c018fb9bbc36befce7ef72fba51abb5e124aadf
Chris Greff <Vehementus@gmail.com>
2013-04-14 13:53:43 -0400

    Prepping Postgres & JDBC stuff.

--- GoPServer/src/response/Response.java
+++ GoPServer/src/response/Response.java
@@ -1,34 +1,88 @@
 package response;
 
 //import jdbc for postgres
+import java.sql.*;
+import java.util.Properties;
 
 public abstract class Response {
 	
 	private String userID;
 	private int gameID;
 	private int buildingID;
+	private final String dbURL = "jdbc:postgresql://localhost/test";
 	
-	public Response(String userID, int gameID, int buildingID){
+	public Response(String userID, int gameID, int buildingID) {
 		this.userID = userID;
 		this.gameID = gameID;
 		this.buildingID = buildingID;
+		
+		try {
+			initDBConnection();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public Response(String userID, int gameID){
 		this.userID = userID;
 		this.gameID = gameID;
+		
+		try {
+			initDBConnection();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public Response(String userID){
 		this.userID = userID;
+		
+		try {
+			initDBConnection();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public Response(int gameID){
 		this.gameID = gameID;
+		
+		try {
+			initDBConnection();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void initDBConnection() throws Exception{
+		Class.forName("org.postgresql.Driver");
+			
+		Properties props = new Properties();
+		props.setProperty("user","admin"); //Replace these values with the actual login later.
+		props.setProperty("password", "pass");
+		Connection dbConn = DriverManager.getConnection(dbURL, props);
 	}
 	
 	public void pushToDatabase(){
+		/* Select Example code:
+		 * Statement st = conn.createStatement();
+		 * ResultSet rs = st.executeQuery("SELECT * FROM mytable WHERE column = 500");
+		 * while (rs.next()) { //Must call rs.next() before using 'rs.getString(#)'
+		 * System.out.print("Column 1 returned ");
+		 * System.out.println(rs.getString(1));
+		 * }
+		 * rs.close();
+		 * st.close();
+		 */
 		
+		/* Delete Example code:
+		 * int foovalue = 500;
+		 * PreparedStatement st = conn.prepareStatement("DELETE FROM mytable WHERE columnfoo = ?");
+		 * st.setInt(1, foovalue);
+		 * int rowsDeleted = st.executeUpdate();
+		 * System.out.println(rowsDeleted + " rows deleted");
+		 * st.close();
+		 */
 	}
 	
 	public String getUserID() {
