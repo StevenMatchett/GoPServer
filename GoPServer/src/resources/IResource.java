bcc9304263a196fb54b8d3a7e1e97b688928a5d3
Chris Greff <Vehementus@gmail.com>
2013-04-15 20:04:40 -0400

    Redoing the Resources

--- GoPServer/src/resources/IResource.java
+++
@@ -1,8 +0,0 @@
-package resources;
-
-public interface IResource {
-	
-	public double getProbability();
-	
-	public void setProbability();
-}
---
+++ GoPServer/src/resources/Resources.java
@@ -0,0 +1,14 @@
+package resources;
+
+//Implement this resource any time you need resource probabilities or whatevers.
+public interface Resources {
+
+	//Probability Constants
+	public final float p_Artifact = 0.5f;
+	public final float p_Blueprints = 0.3f;
+	public final float p_Materials = 0.7f;
+	public final float p_Fuel = 0.6f;
+	public final float p_Luxuries = 0.2f;
+	public final float p_Food = 0.8f;
+	
+}
