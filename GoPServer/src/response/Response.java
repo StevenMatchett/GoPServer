a50f0e9b0245d51838e9ebcb88edd8f89f3eb73f
Chris Greff <Vehementus@gmail.com>
2013-04-17 22:24:48 -0400

    Puuuush

--- GoPServer/src/response/Response.java
+++ GoPServer/src/response/Response.java
@@ -89,7 +89,7 @@ public abstract class Response {
 		 * rs.close();
 		 * st.close();
 		 */
-		
+		//Why won't it commit..
 		try {
 			Statement st = dbConn.createStatement();
 			ResultSet rs = st.executeQuery(query); 
