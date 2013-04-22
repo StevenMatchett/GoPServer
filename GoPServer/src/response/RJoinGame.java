36985a22cc55e868528c66408f2f2bb29fa430d0
Adam Minter <adamminter92@gmail.com>
2013-04-22 10:00:17 -0400

    Server will now check to see if a game is full or not. Going through and making things a bit more robust.

--- GoPServer/src/response/RJoinGame.java
+++ GoPServer/src/response/RJoinGame.java
@@ -4,6 +4,9 @@ import gameResources.Player;
 
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class RJoinGame extends Response {
 
@@ -21,9 +24,26 @@ public class RJoinGame extends Response {
 	
 	@Override
 	public void execute(DataOutputStream out) throws IOException {
-		Player newPlayerForGame = new Player(userID, gameID, dbConn);
-		newPlayerForGame.createPlayer();
-		System.out.println(userID + " has been added to game " + gameID);
-		super.execute(out);
+		if(checkForMaxPlayers()){
+			Player newPlayerForGame = new Player(userID, gameID, dbConn);
+			newPlayerForGame.createPlayer();
+			System.out.println(userID + " has been added to game " + gameID);
+		}
+		super.execute(out);	
+	}
+	
+	public boolean checkForMaxPlayers(){
+		try{
+			Statement checkForMaxPlayers = dbConn.createStatement();
+			ResultSet rs = checkForMaxPlayers.executeQuery("SELECT maxplayers,count(*) FROM game a JOIN player b ON (a.game_id = b.game_id) WHERE (game_id = " + gameID + ");");
+			while(rs.next()){
+				if(rs.getInt(1) > rs.getInt(2)){
+					return true;
+				}
+			}
+		}catch(SQLException e){
+			e.printStackTrace();
+		}
+		return false;
 	}
 }
